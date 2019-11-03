81541a9bc755e32c5bbd2511267289adafa33358
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-11-03 20:53:37 +0200

    updated constant

--- app/src/main/java/com/moringaschool/outingapi2/Constants.java
+++ app/src/main/java/com/moringaschool/outingapi2/Constants.java
@@ -5,4 +5,5 @@ import com.moringaschool.outingapi2.BuildConfig;
 public class Constants {
     public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
     public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+    public static final String FIREBASE_CHILD_RESTAURANTS = "events";
 }
