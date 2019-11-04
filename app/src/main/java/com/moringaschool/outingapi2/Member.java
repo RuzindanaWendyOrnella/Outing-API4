aaf5afa365b1132d89118c38577421428395ee76
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-11-04 18:08:24 +0200

    created a new java class to help me save data to firebase

--- app/src/main/java/com/moringaschool/outingapi2/Member.java
+++ app/src/main/java/com/moringaschool/outingapi2/Member.java
@@ -1,4 +1,61 @@
 package com.moringaschool.outingapi2;
 
 public class Member {
+    private String Name;
+    private String Type;
+    private String Description;
+    private String Dresscode;
+    private String Time;
+    private String imageURL;
+
+    public Member() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public  void setName(String name) {
+        Name = name;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getDresscode() {
+        return Dresscode;
+    }
+
+    public void setDresscode(String dresscode) {
+        Dresscode = dresscode;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
